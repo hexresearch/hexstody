5672777002d553994de7774673253eae276101ac
Aminion <aminion@protonmail.com>
2022-05-11 12:47:15 +0300

    add additional fields and conditions at frontend #20

--- hexstody-hot/static/scripts/page/overview.js
+++ hexstody-hot/static/scripts/page/overview.js
@@ -21,7 +21,8 @@ async function initTemplates() {
     historyTemplate = historyTemp.value;
 
     Handlebars.registerHelper('isDeposit', (historyItem) => historyItem.type === "deposit");
-    Handlebars.registerHelper('formatCurrencyValue', function() {
+    Handlebars.registerHelper('isWithdrawal', (historyItem) => historyItem.type === "withdrawal");
+    Handlebars.registerHelper('formatCurrencyValue', function () {
         return formattedCurrencyValue(this.currency, this.value);
     });
 }
