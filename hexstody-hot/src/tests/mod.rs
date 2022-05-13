ad7bb651f13036d2095d6a9960fb3fbcc8817243
Anton Gushcha <ncrashed@protonmail.com>
2022-05-13 22:50:58 +0200

    Refactor test

--- hexstody-hot/src/tests/mod.rs
+++ hexstody-hot/src/tests/mod.rs
@@ -12,7 +12,7 @@ async fn test_simple() {
 }
 
 #[tokio::test]
-async fn test_signup_email() {
+async fn test_auth_email() {
     run_test(|env| async move {
         let user = "aboba@mail.com".to_owned();
         let password = "123456".to_owned();
@@ -33,6 +33,9 @@ async fn test_signup_email() {
             })
             .await
             .expect("Signup");
+
+        let res = env.hot_client.logout().await;
+        assert!(!res.is_ok(), "Logout before signing");
 
         let res = env
             .hot_client
