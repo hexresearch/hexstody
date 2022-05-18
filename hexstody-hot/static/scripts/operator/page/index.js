9f24c8761c1d2d493c5c783441b2661d4304d151
Vladimir Krutkin <krutkinvs@gmail.com>
2022-05-18 03:15:17 +0300

    Operator index page tweaks

--- hexstody-hot/static/scripts/operator/page/index.js
+++ hexstody-hot/static/scripts/operator/page/index.js
@@ -10,32 +10,36 @@ window.addEventListener('load', function () {
         const reader = new FileReader();
         reader.readAsText(file);
         reader.onload = async () => {
-            const privateKeyObj = new window.jscu.Key('pem', reader.result);
-            if (!privateKeyObj.isPrivate) {
-                fileSelectorStatus.className = "text-error";
-                fileSelectorStatus.innerText = "The selected key is not private!";
-            };
-            if (privateKeyObj.isEncrypted) {
+            const keyObj = new window.jscu.Key('pem', reader.result);
+            if (keyObj.isEncrypted) {
                 const password = window.prompt("Enter password");
-                await privateKeyObj.decrypt(password).catch(_ => {
+                await keyObj.decrypt(password).catch(_ => {
                     fileSelectorStatus.className = "text-error";
                     fileSelectorStatus.innerText = "Wrong password";
                 });
             };
-            if (!privateKeyObj.isEncrypted && privateKeyObj.isPrivate) {
-                fileSelectorStatus.className = "text-success";
-                fileSelectorStatus.innerText = "Private key imported successfully!";
+
+            if (!keyObj.isPrivate) {
+                fileSelectorStatus.className = "text-error";
+                fileSelectorStatus.innerText = "The selected key is not private!";
+            } else {
+                let privateKeyJwk = await keyObj.export('jwk').then(() => {
+                    fileSelectorStatus.className = "text-success";
+                    fileSelectorStatus.innerText = "Private key imported successfully!";
+
+                    // const msg = new Uint8Array(32);
+                    // for (let i = 0; i < 32; i++) msg[i] = 0xFF & i;
+
+                    // const sig = await window.jscec.sign(msg, privateKeyJwk, 'SHA-256').catch(error => {
+                    //     fileSelectorStatus.className = "text-error";
+                    //     fileSelectorStatus.innerText = error;
+                    // });
+
+                }).catch(error => {
+                    fileSelectorStatus.className = "text-error";
+                    fileSelectorStatus.innerText = error;
+                });
             };
-
-            // const msg = new Uint8Array(32);
-            // for (let i = 0; i < 32; i++) msg[i] = 0xFF & i;
-            // let privateKeyJwk = await privateKeyObj.export('jwk');
-
-            // const sig = await window.jscec.sign(msg, privateKeyJwk, 'SHA-256').catch(error => {
-            //     fileSelectorStatus.className = "text-error";
-            //     fileSelectorStatus.innerText = error;
-            // });
-
         };
         reader.onerror = function () {
             fileSelectorStatus.className = "text-error";
