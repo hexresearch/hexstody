f0c3280e1d4b54f84aff3a027938e859a272dfd9
Aminion <aminion@protonmail.com>
2022-06-30 16:16:25 +0300

    Add validation #36

--- hexstody-public/src/api/wallet.rs
+++ hexstody-public/src/api/wallet.rs
@@ -1,8 +1,9 @@
 use log::*;
 use std::sync::Arc;
 use tokio::sync::{mpsc, Mutex};
+use uuid::Uuid;
 
-use rocket::http::{CookieJar, Status};
+use rocket::http::CookieJar;
 use rocket::serde::json::Json;
 use rocket::{get, post, State};
 use rocket_okapi::openapi;
@@ -11,7 +12,7 @@ use super::auth::require_auth;
 use hexstody_api::domain::{BtcAddress, Currency, CurrencyAddress};
 use hexstody_api::error;
 use hexstody_api::types as api;
-use hexstody_btc_client::client::BtcClient;
+use hexstody_btc_client::client::{BtcClient, BTC_BYTES_PER_TRANSACTION};
 use hexstody_db::state::State as DbState;
 use hexstody_db::state::WithdrawalRequestStatus;
 use hexstody_db::state::{Transaction, WithdrawalRequest, REQUIRED_NUMBER_OF_CONFIRMATIONS};
@@ -168,6 +169,7 @@ use hexstody_db::update::withdrawal::WithdrawalRequestInfo;
 #[post("/withdraw", data = "<withdraw_request>")]
 pub async fn post_withdraw(
     cookies: &CookieJar<'_>,
+    btc: &State<BtcClient>,
     updater: &State<mpsc::Sender<StateUpdate>>,
     state: &State<Arc<Mutex<DbState>>>,
     withdraw_request: Json<api::UserWithdrawRequest>,
@@ -176,18 +178,36 @@ pub async fn post_withdraw(
         let user_id = cookie.value();
         {
             let state = state.lock().await;
-            if let Some(_) = state.users.get(user_id) {
-                let withdrawal_request = WithdrawalRequestInfo {
-                    user: user_id.to_owned(),
-                    address: withdraw_request.address.to_owned(),
-                    amount: withdraw_request.amount,
-                };
-                let state_update =
-                    StateUpdate::new(UpdateBody::NewWithdrawalRequest(withdrawal_request));
-                updater
-                    .send(state_update)
+            if let Some(user) = state.users.get(user_id) {
+                let btc_fee_per_byte = &btc
+                    .get_fees()
                     .await
-                    .map_err(|_| error::Error::NoUserFound.into())
+                    .map_err(|_| error::Error::FailedGetFee(Currency::BTC))?
+                    .fee_rate;
+
+                let btc_balance = &user
+                    .currencies
+                    .get(&Currency::BTC)
+                    .ok_or(error::Error::NoUserCurrency(Currency::BTC))?
+                    .finalized_balance();
+                let max_btc_amount_to_spend =
+                    btc_balance - (btc_fee_per_byte * BTC_BYTES_PER_TRANSACTION) as i64;
+                if max_btc_amount_to_spend >= withdraw_request.amount as i64 {
+                    let withdrawal_request = WithdrawalRequestInfo {
+                        id: Uuid::new_v4(),
+                        user: user_id.to_owned(),
+                        address: withdraw_request.address.to_owned(),
+                        amount: withdraw_request.amount,
+                    };
+                    let state_update =
+                        StateUpdate::new(UpdateBody::CreateWithdrawalRequest(withdrawal_request));
+                    updater
+                        .send(state_update)
+                        .await
+                        .map_err(|_| error::Error::NoUserFound.into())
+                } else {
+                    Err(error::Error::InsufficientFounds(Currency::BTC).into())
+                }
             } else {
                 Err(error::Error::NoUserFound.into())
             }
