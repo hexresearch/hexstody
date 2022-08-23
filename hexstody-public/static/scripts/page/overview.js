775ae72ce1e95b62d764c194ce18129c0b1bcb68
Aminion <aminion@protonmail.com>
2022-08-23 17:03:33 +0300

    Fix tipo #128

--- hexstody-public/static/scripts/page/overview.js
+++ hexstody-public/static/scripts/page/overview.js
@@ -113,7 +113,7 @@ async function loadHistoryETH() {
 
     const hist = { histories: histFull }
     for (const h of hist.histories) {
-        f.timeStamp = timeStampToTime(parseInt(h.timeStamp));
+        h.timeStamp = timeStampToTime(parseInt(h.timeStamp));
         const isDeposit = h.addr.toUpperCase() != h.from.toUpperCase();
         const sign = isDeposit ? '+' : '-';
         h.valuetoshow = `${sign + formattedCurrencyValue(h.tokenName, h.value)} ${h.tokenName}`;
