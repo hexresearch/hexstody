167203ec1b4dbf648a6ad64bde220eca01888353
Vladimir Krutkin <krutkinvs@gmail.com>
2022-08-25 16:42:36 +0300

    GTECH contract address fix

--- hexstody-api/src/domain/currency.rs
+++ hexstody-api/src/domain/currency.rs
@@ -44,7 +44,7 @@ impl Currency {
         Currency::ERC20(Erc20Token {
             ticker: "GTECH".to_string(),
             name: "GTECH".to_string(),
-            contract: "0xD533a949740bb3306d119CC777fa900bA034cd52".to_string(),
+            contract: "0xf4904d8c93d7Fd72eff00f7F7A1195E9875da4D1".to_string(),
         })
     }
 
