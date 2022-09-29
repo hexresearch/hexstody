51fb4b02a796e2a71377de5acecaaeecefdfdcb4
nazgull08 <nazgull08@protonmail.com>
2022-09-29 20:13:20 +0300

    added mainnet runner scripts

--- regtest.sh
+++ regtest.sh
@@ -1,4 +1,6 @@
 export RUST_LOG=debug
 cargo run --bin hexstody-hot -- \
 	--operator-public-keys operator1-key.pub.pem operator2-key.pub.pem \
+	--public-api-domain https://demo.desolator.net \
+	--operator-api-domain https://operator.desolator.net \
 	--start-regtest serve
